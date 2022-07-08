c08c42119bdc2a863ba6720f926c4cce0863dc25
jenil <jenilsavani1@gmail.com>
2022-07-08 18:29:25 +0530

    bookvilla code commit

---
+++ backend/BookStore.Models/Models/RoleModel.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Models.DataModels;
+
+namespace BookStore.Models.Models
+{
+    public class RoleModel
+    {
+        public RoleModel(Role role) { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
